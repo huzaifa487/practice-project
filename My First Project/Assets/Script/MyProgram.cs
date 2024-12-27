d4af1befe7dfee58457b4e74d951cdfc22f01a95
huzaifa487 <huzaifasalar487@gmail.com>
2024-12-27 23:57:49 +0500

    MAIN MENU to game logic done

---
+++ My First Project/Assets/Script/MyProgram.cs
@@ -0,0 +1,17 @@
+using UnityEngine;   //using ka mtlb hota h k ye aik library use kr raha h AUR US LIBRARY KA NAME H unity engine
+
+public class MyProgram : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()  // this is called function
+    {
+        // ye function game k start me sirf aik dfa chlta h
+        
+    }
+
+    // Update is called once per frame
+    void Update()  //this is called function
+    {
+        //ye function poori game me hr frame me 1 dfa chlta h
+    }
+}
